6b1dad55afbcfd0b51f331c1371edd101917040f
Dean Keeble <dean.keeble@diamond.ac.uk>
2020-10-06 16:22:16 +0100

    simple file loader test

--- tests/test_rmcalyse.py
+++ tests/test_rmcalyse.py
@@ -8,30 +8,11 @@ from click.testing import CliRunner
 
 from rmcalyse import rmcalyse
 from rmcalyse import cli
+from rmcalyse.general_functions.read_rmc6f import read_in
 
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'rmcalyse.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+def test_rmc6f_file_reader():
+    # really simple (probably too simplistic!) pytest
+    (cell, atoms) = read_in('SrTiO3_00Nb.rmc6f')
+    expected = [19.66995, 19.66995, 19.66995, 90.0, 90.0, 90.0]
+    assert all(a == pytest.approx(b) for a,b in zip(cell, expected))
+    assert len(atoms) == 625
