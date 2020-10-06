7b939ea2c841657e870318c5a2d9b4f5ce7ae18d
AGB09 <antongb09@gmail.com>
2020-10-06 14:33:46 +0100

    reverted distance_calculator.py to basic calculation

--- rmcalyse/general_functions/distance_calculator.py
+++ rmcalyse/general_functions/distance_calculator.py
@@ -5,6 +5,6 @@ def distance_calc(coordA, coordB):
     '''
     Calculates distance between two coordinates in an orthnomal basis.
     '''
-    int_dist = np.linalg(np.array(coordA) - np.array(coordB))
-
+    int_dist = np.around((sum(
+        (np.array(coordA) - np.array(coordB))**2)**0.5), 4)
     return int_dist
