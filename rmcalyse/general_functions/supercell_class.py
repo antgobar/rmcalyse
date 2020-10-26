bedfff5211e95b58fffb22a48c8bcb1b39a79161
AGB09 <antongb09@gmail.com>
2020-10-26 12:01:27 +0000

    SuperCell class reads file once, added test, fixed red_in test

--- rmcalyse/general_functions/supercell_class.py
+++ rmcalyse/general_functions/supercell_class.py
@@ -9,18 +9,21 @@ class SuperCell():
     
     def __init__(self, file_path):
         self.file_path = file_path
+        self.cell_parameters = None
+        self.elements = None
+        self.atom_list = None
+        self.supercell_size = None
+        self.density = None
+        
 
     def get_data(self):
-        rmc6f_data =  open(self.file_path, 'r')
+        with open(self.file_path, 'r') as f:
+            rmc_data = f.readlines()
 
-        line_elements = []
-        line_density = []
-        line_supercell = []
-        line_cell = []
-        line_atom_list = []
+        atom_list_lines = []
 
-        # Loop through file to find matchin strings
-        for line in rmc6f_data:
+        # Loop through file to find matching strings
+        for line in rmc_data:
             # Elements
             if line.find('Atom types present:') >= 0:
                 line_elements = line
@@ -29,17 +32,16 @@ class SuperCell():
                 line_density = line
             # Supercell dimensions
             if line.find('Supercell') >= 0:
-                line_supercell = line
+                line_supercell_size = line
             # Supercell parameters 
             if line.find('Cell') >= 0:
                 line_cell = line
             # Atom list
             if line.find('[1]') >= 0:
-                line_atom_list.append(line)
-
-        rmc6f_data.close()
+                atom_list_lines.append(line)
 
         # Put element, atom no. and atomic positions into lists
+
         # Elements
         key = 'Atom types present:'
         lead_str, keyword, elements_str = line_elements.partition(key)
@@ -51,15 +53,15 @@ class SuperCell():
         density = [float(i) for i in temp]        
 
         # supercell dimensions - currently not used
-        temp = re.findall('[-+]?\d*\.\d+|\d+', line_supercell)
-        supercell = [int(i) for i in temp]
+        temp = re.findall('[-+]?\d*\.\d+|\d+', line_supercell_size)
+        supercell_size = [int(i) for i in temp]
 
         # supercell parameters: a, b, c, alpha, beta, gamma
         temp = re.findall('[-+]?\d*\.\d+|\d+', line_cell)
         cell_parameters = [float(i) for i in temp]
 
         atom_list = []
-        for line in line_atom_list:
+        for line in atom_list_lines:
             temp = []
             temp_1 = re.findall(r'\b\d+\b', line)  # ints
             temp_2 = re.findall('[a-zA-Z]+', line)  # letters
@@ -71,18 +73,12 @@ class SuperCell():
             temp.append(float(temp_3[2]))
             atom_list.append(temp)
 
-               
-        return cell_parameters, atom_list, elements
-
-    def cell_parameters(self):
-        return self.get_data()[0]
-
-    def elements(self):
-        return self.get_data()[3]
-
-    def elements(self):
-        return self.get_data()[3]
+        self.cell_parameters = cell_parameters
+        self.elements = elements
+        self.atom_list = atom_list
+        self.supercell_size = supercell_size
+        self.density = density
 
     
-path2 = '/Users/Anton/Documents/Python_Programs/rmcalyse/rmcalyse/read_in/STO_2.rmc6f'
+#path2 = '/Users/Anton/Documents/Python_Programs/rmcalyse/rmcalyse/read_in/STO_2.rmc6f'
 
--- tests/test_read_in.py
+++ tests/test_read_in.py
@@ -12,7 +12,7 @@ from rmcalyse.general_functions.read_rmc6f import read_in
 
 def test_rmc6f_file_reader():
     # really simple (probably too simplistic!) pytest
-    (cell, atoms) = read_in('tests/SrTiO3_00Nb.rmc6f')
+    cell, atoms, elements = read_in('tests/SrTiO3_00Nb.rmc6f')
     expected = [19.66995, 19.66995, 19.66995, 90.0, 90.0, 90.0]
     assert all(a == pytest.approx(b) for a,b in zip(cell, expected))
     assert len(atoms) == 625
