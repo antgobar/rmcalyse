431ecf2dc4aea19fdadcedeefddd01978efeb158
AGB09 <antongb09@gmail.com>
2021-02-05 23:08:28 +0000

    added stereographic projection net to represent pseudocubic crystallographic directions

---
+++ rmcalyse/plotting_functions/plot_stereographic_projection_net.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sp_net(radius):
+    base_angle = np.linspace(-90, 90, 100)
+    base_length = 1 / np.cos(base_angle * np.pi / 180)
+    angle2point = (180 / np.pi) * np.arctan(1/base_length)
+    angle_inner = 45 - angle2point* 0.5
+    opposite = radius * np.tan(angle_inner * np.pi / 180)
+
+    x = opposite * np.cos(base_angle * np.pi / 180)
+    y = opposite * np.sin(base_angle * np.pi / 180)
+
+    diag = radius * np.cos(np.pi/4)
+    diag_m = np.linspace(-diag, diag)
+
+    orth = np.linspace(-radius, radius, 2)
+
+    plt.plot(x, y, c='k')
+    plt.plot(-x, y, c='k')
+    plt.plot(y, x, c='k')
+    plt.plot(y, -x, c='k')
+
+    plt.plot(diag_m, diag_m, c='k')
+    plt.plot(diag_m, -diag_m, c='k')
+
+    plt.plot(np.zeros(2), orth, c='k')
+    plt.plot(orth, np.zeros(2), c='k')
+
+    circle1 = plt.Circle((0, 0), radius, color='k', fill = False)
+    plt.gca().add_patch(circle1)
+
+    plt.gca().set_aspect('equal', adjustable='box')
+    #plt.show()
