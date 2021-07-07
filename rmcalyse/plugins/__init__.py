a46fe7f66a25d20ff5bf62da91f812bd047b9243
Dean Keeble <dean.keeble@diamond.ac.uk>
2021-07-07 12:42:57 +0100

    added second examplee

--- rmcalyse/plugins/__init__.py
+++ rmcalyse/plugins/__init__.py
@@ -4,5 +4,6 @@ from .centroids import Centroider
 from .clustering import Clusterer
 from .volume_plotter import VolumePlotter
 from .example import Exampler
+from .examplee import Examplere
 
 from .base_plugin import PluginFactory
